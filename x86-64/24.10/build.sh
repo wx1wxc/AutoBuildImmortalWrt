36ac9d5f03b5e71e4e77c1aa69b6888051b4a779
wx1wxc <wx1wxc@users.noreply.github.com>
2025-01-03 16:17:41 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -28,6 +28,7 @@ PACKAGES="$PACKAGES ddns-scripts-cloudflare"
 PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
 PACKAGES="$PACKAGES luci-app-vlmcsd"
 PACKAGES="$PACKAGES luci-i18n-attendedsysupgrade-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-wol-zh-cn"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
