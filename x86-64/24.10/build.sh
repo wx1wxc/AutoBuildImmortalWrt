c67d130b70db4ffb7a208bdb8c705c6503bb8501
wx1wxc <wx1wxc@users.noreply.github.com>
2025-01-28 14:46:32 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -31,7 +31,7 @@ PACKAGES="$PACKAGES luci-i18n-attendedsysupgrade-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-wol-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-cifs-mount-zh-cn"
-PACKAGES="$PACKAGES luci-app-mihomo"
+#PACKAGES="$PACKAGES luci-app-mihomo"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
